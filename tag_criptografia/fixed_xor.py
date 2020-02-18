a1c8239459deaf93e307c7c6fbe2ea57919f7e91
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-02-18 11:28:16 -0300

    Deixa todos os bytes com 4 bits

--- tag_criptografia/fixed_xor.py
+++ tag_criptografia/fixed_xor.py
@@ -7,24 +7,34 @@ def fixed_xor(entrada_1, entrada_2):
 
 	tam_hex_num = len(hex_list_1)
 
-	bin_list_1 = [0] * tam_hex_num * 4
-	bin_list_2 = [0] * tam_hex_num * 4
+	xor_list = [0] * tam_hex_num * 4
 
 	for count in range(tam_hex_num):
 		# converte a string hexa da primeira entrada em decimal
 		try:
-			this_hex_num_1 = int(hex_list_1[count], 16)
+			this_byte_num_1 = list(bin(int(hex_list_1[count], 16)))
 		except:
-			this_hex_num_1 = int(hex_list_1[count])
+			this_byte_num_1 = list(bin(int(hex_list_1[count])))
 
 		# converte a string hexa da segunda entrada em decimal
 		try:
-			this_hex_num_2 = int(hex_list_2[count], 16)
+			this_byte_num_2 = list(bin(int(hex_list_2[count], 16)))
 		except:
-			this_hex_num_2 = int(hex_list_2[count])	
+			this_byte_num_2 = list(bin(int(hex_list_2[count])))	
 
-		print(this_hex_num_1)
-		print(this_hex_num_2)
+		this_byte_num_1.pop(0)
+		this_byte_num_1.pop(0)
+		this_byte_num_2.pop(0)
+		this_byte_num_2.pop(0)
+	
+		for tamanho in range(4 - len(this_byte_num_1)):
+			this_byte_num_1.insert(0, '0')
+
+		for tamanho in range(4 - len(this_byte_num_2)):
+			this_byte_num_2.insert(0, '0')
+
+		print(this_byte_num_1)
+		print(this_byte_num_2)
 
 	return 0	
 
