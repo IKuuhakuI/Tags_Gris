d8c6f6b105afb4f0f659478540311f50b6a63569
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-02-18 13:21:26 -0300

    cria uma funcao para o xor

--- tag_criptografia/single_xor.py
+++ tag_criptografia/single_xor.py
@@ -1,4 +1,16 @@
-def single_byte_xor(entrada):
+def single_xor(entrada, caractere, tamanho):
+	xor_byte_list = []
+
+	for count in range(tamanho):
+		if entrada[count] != caractere[count]:
+			xor_byte_list.append('1')
+		
+		else:
+			xor_byte_list.append('0')
+
+	return xor_byte_list	
+
+def decifrar(entrada):
 	dic = {}
 	max_list = ["", 0]
 
@@ -33,10 +45,10 @@ def single_byte_xor(entrada):
 	for count in range(8 - len(max_key)):
 		max_key.insert(0,'0')
 
-	print(max_key)
-
 	# e = 0x65
 	letra_e = ['0', '1', '0', '0', '0', '0', '0', '1'] 
-		
+	
+	xor_byte_list = single_xor(max_key, letra_e, 8)
 
-single_byte_xor('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+	print(xor_byte_list)
+decifrar('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
