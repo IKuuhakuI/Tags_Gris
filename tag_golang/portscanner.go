437eb8c7ad91d4679e8b33f7560d6ee3463ff4c4
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-02-27 23:26:30 -0300

    Faz o loop para testar todas as portas

--- tag_golang/portscanner.go
+++ tag_golang/portscanner.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("Teste")
+
+	qtd_ports := 65535
+
+	for i := 1; i < qtd_ports; i++ {
+		fmt.Println(strconv.FormatInt(int64(i), 10))
+	}
 }
