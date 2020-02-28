1bfc08a52dad35f3a31d89bde1e85bd0cbf238d1
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-02-27 23:34:06 -0300

    Port Scan sem multithreading

--- tag_golang/portscanner.go
+++ tag_golang/portscanner.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"net"
 )
 
 func main() {
@@ -11,6 +12,12 @@ func main() {
 	qtd_ports := 65535
 
 	for i := 1; i < qtd_ports; i++ {
-		fmt.Println(strconv.FormatInt(int64(i), 10))
+		porta_atual := strconv.FormatInt(int64(i), 10)
+		conn, erro := net.Dial("tcp", "127.0.0.1:" + porta_atual)
+
+		if erro == nil {
+			fmt.Println("Porta", i, "aberta")
+			conn.Close()
+		}
 	}
 }
